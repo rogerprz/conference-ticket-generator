5ea1f950895cfa078ad42f4ebe1fe59fb0af29c3
Roger Perez <ropere@microsoft.com>
2025-03-16 15:04:42 -0700

    add generate ticket

---
+++ index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('generate-ticket-button');
+  console.log('BBB:', button);
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('full-name');
+    const username = document.getElementById('github-username');
+    const email = document.getElementById('email');
+    const avatar = document.getElementById('upload-avatar');
+    const nameValue = name.value;
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const avatarValue = avatar.value;
+    const avatarFile = avatar.files[0];
+    console.log(nameValue);
+    console.log(usernameValue);
+    console.log(avatar, avatarFile, avatarValue);
+
+    const dateOneMonthFromNow = new Date();
+    dateOneMonthFromNow.setMonth(dateOneMonthFromNow.getMonth() + 1);
+    const header = document.getElementById('h1-header');
+    const subHeader = document.getElementById('h4-sub-header');
+    const ticket = document.getElementById('ticket-section');
+    header.innerHTML =
+      ticket.innerHTML = `Congrats, <span style="background: linear-gradient(to right, orange, white); -webkit-background-clip: text; color: transparent;">${nameValue}</span>! Your ticket is ready.`;
+    subHeader.innerHTML = `We've emailed your ticket to ${emailValue} and will send updates in the run up to the event.`;
+
+    ticket.innerHTML = `
+      <div>
+        <img src="./assets/images/logo-full.svg" alt="Coding conf logo" />
+        <div class="ticket-info">${dateOneMonthFromNow} / Seattle, WA</div>
+      </div>
+      <div>
+        <h2 class="ticket-info">${nameValue}</h2>
+        <h3 class="ticket-info">${usernameValue}</h3>
+      </div>
+      `;
+
+    const formSection = document.getElementById('ticket-form');
+    formSection.style.display = 'none';
+    ticket.style.display = 'block';
+  });
+});
