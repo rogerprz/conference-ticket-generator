4a805d2406dc017573104c279e0f9409fdf1b946
Roger Perez <ropere@microsoft.com>
2025-04-05 12:52:05 -0700

    add error handling for images

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let uploadAvatar = null;
 document.addEventListener('DOMContentLoaded', function () {
-  const button = document.getElementById('generate-ticket-button');
-  uploadAvatar = document.getElementById('upload-avatar');
+  const genButton = document.getElementById('generate-ticket-button');
+  const uploadAvatar = document.getElementById('upload-avatar');
   let avatarImage = null;
 
   uploadAvatar.addEventListener('change', function () {
     if (uploadAvatar.files.length > 0) {
-      button.style.cursor = 'pointer';
-      button.style.backgroundColor = 'rgb(214, 114, 97)';
-      button.disabled = false;
+      genButton.style.cursor = 'pointer';
+      genButton.style.backgroundColor = 'rgb(214, 114, 97)';
+      genButton.disabled = false;
     }
   });
 
-  button.disabled = true;
-  button.style.cursor = 'not-allowed';
-  button.style.backgroundColor = 'gray';
+  genButton.disabled = true;
+  genButton.style.cursor = 'not-allowed';
+  genButton.style.backgroundColor = 'gray';
 
-  button.addEventListener('click', function (event) {
+  genButton.addEventListener('click', function (event) {
     event.preventDefault();
     const name = document.getElementById('full-name');
     const username = document.getElementById('github-username');
@@ -60,6 +59,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
   uploadAvatar.addEventListener('change', function (event) {
     const file = event.target.files[0];
+    console.log('File:', file);
+    const fileSize = file.size ? Math.round(file.size / 1024) : 0;
+    if (fileSize > 500) {
+      const uploadYourPhoto = document.getElementById('upload-your-photo');
+
+      uploadYourPhoto.innerHTML = `
+         <img id="icon-error" src="./assets/images/icon-info.svg" alt="error">
+        <span style="color: red;">File too large. Please upload a file under 500KB.</span`;
+      return;
+    }
+
+    if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
+      const uploadYourPhoto = document.getElementById('upload-your-photo');
+      uploadYourPhoto.innerHTML = `
+        <img id="icon-error" src="./assets/images/icon-info.svg" alt="error">
+        <span style="color: red;">File type not supported. Please upload a PNG or JPEG file.</span>`;
+      return;
+    }
+
     const reader = new FileReader();
     console.log('File:', file);
     console.log('Reader:', reader);
@@ -84,6 +102,55 @@ document.addEventListener('DOMContentLoaded', function () {
       reader.readAsDataURL(file);
     }
   });
+
+  const uploadChangeAvatarContainer = () => {
+    const divContainer = document.createElement('div');
+    divContainer.classList.add('avatar-buttons-container');
+
+    const removeImage = document.createElement('button');
+    removeImage.innerText = 'Remove Image';
+
+    const changeImage = document.createElement('button');
+    changeImage.innerText = 'Change Image';
+
+    divContainer.appendChild(removeImage);
+    divContainer.appendChild(changeImage);
+    divContainer.classList.add('avatar-buttons');
+
+    removeImage.addEventListener('click', function () {
+      // TODO: Remove image needs to revert to old render
+      // We need to capture old render and allow new image to be uploaded
+      const newAvatar = document.getElementById('avatar-container');
+      console.log('Upload Avatar:', newAvatar);
+      const label = document.createElement('label');
+
+      label.setAttribute('for', 'upload-avatar');
+      label.classList.add('avatar');
+      label.accept = 'image/png, image/jpeg';
+      label.ariaLabel = 'Upload your avatar';
+      label.innerHTML = `
+        <input type="file" id="upload-avatar" accept="image/png, image/jpeg" hidden required>
+        <div class="upload-box">
+          <img src="./assets/images/icon-upload.svg" alt="Upload Avatar image">
+          <div class="sub-text upload-text">Drag and drop or click to upload</div>
+        </div>
+        `;
+      newAvatar.replaceWith(label);
+      uploadAvatar = label;
+    });
+
+    changeImage.addEventListener('click', function () {
+      const avatar = document.getElementById('avatar-image');
+      const uploadAvatar = document.createElement('input');
+      uploadAvatar.type = 'file';
+      uploadAvatar.id = 'upload-avatar';
+      uploadAvatar.accept = 'image/*';
+      avatar.parentNode.replaceWith(uploadAvatar);
+      divContainer.remove();
+    });
+
+    return divContainer;
+  };
 });
 
 const avatarImage = (src) => {
@@ -93,55 +160,6 @@ const avatarImage = (src) => {
   avatarImage.style.width = '100px';
   avatarImage.style.height = '100px';
   return avatarImage;
-};
-
-const uploadChangeAvatarContainer = () => {
-  const divContainer = document.createElement('div');
-  divContainer.classList.add('avatar-buttons-container');
-
-  const removeImage = document.createElement('button');
-  removeImage.innerText = 'Remove Image';
-
-  const changeImage = document.createElement('button');
-  changeImage.innerText = 'Change Image';
-
-  divContainer.appendChild(removeImage);
-  divContainer.appendChild(changeImage);
-  divContainer.classList.add('avatar-buttons');
-
-  removeImage.addEventListener('click', function () {
-    // TODO: Remove image needs to revert to old render
-    // We need to capture old render and allow new image to be uploaded
-    const newAvatar = document.getElementById('avatar-container');
-    console.log('Upload Avatar:', newAvatar);
-    const label = document.createElement('label');
-
-    label.setAttribute('for', 'upload-avatar');
-    label.classList.add('avatar');
-    label.accept = 'image/png, image/jpeg';
-    label.ariaLabel = 'Upload your avatar';
-    label.innerHTML = `
-        <input type="file" id="upload-avatar" accept="image/png, image/jpeg" hidden required>
-        <div class="upload-box">
-          <img src="./assets/images/icon-upload.svg" alt="Upload Avatar image">
-          <div class="sub-text upload-text">Drag and drop or click to upload</div>
-        </div>
-        `;
-    newAvatar.replaceWith(label);
-    uploadAvatar = label;
-  });
-
-  changeImage.addEventListener('click', function () {
-    const avatar = document.getElementById('avatar-image');
-    const uploadAvatar = document.createElement('input');
-    uploadAvatar.type = 'file';
-    uploadAvatar.id = 'upload-avatar';
-    uploadAvatar.accept = 'image/*';
-    avatar.parentNode.replaceWith(uploadAvatar);
-    divContainer.remove();
-  });
-
-  return divContainer;
 };
 
 const congratsMessage = (name) => {
